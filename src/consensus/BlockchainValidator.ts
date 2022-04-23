a108d06fcc25d8a14cae0e9717dcde693ff6fe8e
Yushi Sato <yushi.kinger@gmail.com>
2022-04-24 01:41:23 +0900

    blockchain validator implmented. - with journaling, snapshot, revert

--- src/consensus/BlockchainValidator.ts
+++ src/consensus/BlockchainValidator.ts
@@ -4,67 +4,209 @@ import { BlockValidator } from "./BlockValidator";
 import { ConsensusEngine } from "./ConsensusEngine"
 import { TransactionVerifier } from "./TransactionVerifier";
 
+class Journal {
+    address: string
+    amount: bigint
+    positive: boolean
+    constructor(address: string, amount: bigint, positive: boolean) {
+        if (amount < 0) {
+            throw new Error('Journal amount must be non-negative')
+        }
+        this.address = address
+        this.amount = amount
+        this.positive = positive
+    }
+}
+
+class BalanceMapCache {
+    private balances: {[address: string]: bigint} = {}
+    private journals: Journal[]
+
+    constructor(private upto: number) {
+        this.balances = {}
+        this.journals = []
+    }
+
+    static Empty = (): BalanceMapCache => {
+        return new BalanceMapCache(0)
+    }
+
+    getUpTo = (): number => {
+        return this.upto
+    }
+    setUpTo = (upto: number) => {
+        this.upto = upto
+    }
+
+    getBalance = (address: string): bigint => {
+        return this.balances[address]
+    }
+    addBalance = (address: string, amount: bigint) => {
+        this.balances[address] += amount
+        this.journals.push(new Journal(address, amount, true))
+    }
+    subtractBalance = (address: string, amount: bigint) => {
+        this.balances[address] -= amount
+        this.journals.push(new Journal(address, amount, false))
+    }
+
+    takeSnapshot = (): number => {
+        return this.journals.length
+    }
+
+    revert = (snapshot: number) => {
+        const len = this.journals.length
+        for (let i = len - 1; i >= snapshot; i--) {
+            const journal = this.journals[i]
+            if (journal.positive) {
+                this.balances[journal.address] -= journal.amount
+            }else{
+                this.balances[journal.address] += journal.amount
+            }
+            this.journals.pop()
+        }
+    }
+}
+
+export class ChainValidationError {
+    height: number = 0
+    transactionIndex: number = 0
+    transactionHash: string = Buffer.allocUnsafe(32).fill(0).toString('hex')
+    message: string = ''
+
+    constructor(height: number, transactionIndex: number, transactionHash: string, message: string){
+        this.height = height
+        this.transactionIndex = transactionIndex
+        this.transactionHash = transactionHash
+        this.message = message
+    }
+}
+
 export class BlockchainValidator {
-    private validator: BlockValidator
+    private cache: BalanceMapCache = BalanceMapCache.Empty()
 
-    constructor(verifier: TransactionVerifier, consensusEngine: ConsensusEngine) {
-        this.validator = new BlockValidator(verifier, consensusEngine)
+    constructor(private verifier: TransactionVerifier, private consensusEngine: ConsensusEngine) {
+
     }
 
-    tryAppendBlock = (blockchain: Blockchain, block: Block): {ok: boolean, message: string} => {
+    tryAppendBlock = (blockchain: Blockchain, block: Block): ChainValidationError|undefined => {
+        //  if balance map not in sync with given blockchain
+        if (blockchain.blocks.length > this.cache.getUpTo()) {
+            const error = this.validateMissingChain(blockchain)
+            if (error) {
+                return error
+            }
+        }
+
+        const blockError = this.validateBlockConsensus(block)
+        if (blockError) {
+            //  block error doesn't set transaction index
+            return new ChainValidationError(blockchain.blocks.length, -1, '', blockError)
+        }
+
+        const snapshot = this.cache.takeSnapshot()
+        const error = this.validateBlockTransactions(block)
+        if (error) {
+            this.cache.revert(snapshot)
+            const hashString = block.getTransactions()[error.index].hashString()
+            return new ChainValidationError(blockchain.blocks.length, error.index, hashString, error.message)
+        }
+
         blockchain.blocks.push(block)
-        const { ok, } = this.validateEntireChain(blockchain)
-        if (!ok) {
-            blockchain.blocks.pop()   // remove invalid block just added
-            return { ok: false, message: 'new block violates consensus' }
-        }
-
-        return { ok: true, message: '' }
+        this.cache.setUpTo(blockchain.blocks.length)
     }
 
-    validateEntireChain = (blockchain: Blockchain): {ok: boolean, message: string} => {
-        let balances: {[address: string]: bigint} = {}
+    validateMissingChain = (blockchain: Blockchain): ChainValidationError|undefined => {
+        for (let height = this.cache.getUpTo(); height < blockchain.blocks.length; height++) {
+            const block = blockchain.blocks[height]
 
-        for (const block of blockchain.blocks) {
-            if (!this.validator.validate(block)) {
-                return { ok: false, message: `validation failed at height ${block.getHeight()}`}
+            const blockError = this.validateBlockConsensus(block)
+            if (blockError) {
+                //  block error doesn't set transaction index
+                return new ChainValidationError(height, -1, '', blockError)
             }
 
-            for (const tx of block.getTransactions()) {
-                let miner: string = ''
-                if (tx.isCoinbase()) {
-                    const dest = tx.getDests()[0]
-                    miner = dest.getAddressString()
+            const snapshot = this.cache.takeSnapshot()
+            const error = this.validateBlockTransactions(block)
+            if (error) {
+                this.cache.revert(snapshot)
+                const hashString = block.getTransactions()[error.index].hashString()
+                return new ChainValidationError(height, error.index, hashString, error.message)
+            }
+            this.cache.setUpTo(height + 1)
+        }
+    }
 
-                    // miner receives mined token
-                    balances[miner] += dest.getAmount()
-                    continue
+    validateEntireChainFromZero = (blockchain: Blockchain): ChainValidationError|undefined => {
+        this.cache = BalanceMapCache.Empty()
+        return this.validateMissingChain(blockchain)
+    }
+
+    private validateBlockTransactions = (block: Block): {index: number, message: string}|undefined => {
+        for (let i = 0; i < block.getTransactions().length; i++) {
+            const tx = block.getTransactions()[i]
+
+            if (!this.verifier.verifySignature(tx)) {
+                return { index: i, message: 'invalid signature' }
+            }
+
+            if (!this.verifier.verifyConsensus(tx)) {
+                return { index: i, message: 'consensus rule violated' }
+            }
+
+            let miner: string = ''
+            if (tx.isCoinbase()) {
+                const dest = tx.getDests()[0]
+                miner = dest.getAddressString()
+
+                // miner receives mined token
+                this.cache.addBalance(miner, dest.getAmount())
+                continue
+            }
+
+            let sum: bigint = BigInt(0)
+            for (const dest of tx.getDests()) {
+                sum += dest.getAmount()
+
+                //  recipient receives sent token
+                this.cache.addBalance(dest.getAddressString(), dest.getAmount())
+            }
+
+            //  miner receives fee
+            this.cache.addBalance(miner, tx.getFee())
+
+            //  sender sends sum and fee
+            const sender = tx.getFromAddressString()
+            this.cache.subtractBalance(sender, sum + tx.getFee())
+
+            //  sender's balance must be non-negative
+            if (this.cache.getBalance(sender) < 0) {
+                return { index: i, message: 'sender tried to send too much amount' }
+            }
+        }
+    }
+
+    private validateBlockConsensus = (block: Block): string|undefined => {
+        if (!this.consensusEngine.verifyDifficulty(block)) {
+            return `invalid difficulty`;
+        }
+
+        let coinbaseCount = 0
+        for (let i = 0; i < block.getTransactions().length; i++) {
+            const tx = block.getTransactions()[i]
+
+            if (tx.isCoinbase()) {
+                if (i != 0) {
+                    // coinbase must be at the top of the block
+                    return `coinbase transaction not at the top`
                 }
-
-                let sum: bigint = BigInt(0)
-                for (const dest of tx.getDests()) {
-                    sum += dest.getAmount()
-
-                    //  recipient receives sent token
-                    balances[dest.getAddressString()] += dest.getAmount()
-                }
-
-                //  miner receives fee
-                balances[miner] += tx.getFee()
-
-                //  sender sends sum and fee
-                const sender = tx.getFromAddressString()
-                balances[sender] -= sum + tx.getFee()
-
-                //  sender's balance must be non-negative
-                if (balances[sender] < 0) {
-                    return { ok: false, message: `tx ${tx.hash()} tried to send too much amount`}
-                }
+                coinbaseCount += 1
             }
         }
 
-        return { ok: true, message: '' }
+        //  only one coinbase is allowed
+        if (coinbaseCount != 1) {
+            return `only one coinbase transaction is allowed`
+        }
     }
-
-    
 }
