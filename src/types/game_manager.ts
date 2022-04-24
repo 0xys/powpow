5ef819240856bcb24799ab3f15fee956c4a8f016
Yushi Sato <yushi.kinger@gmail.com>
2022-04-24 16:49:49 +0900

    game_manager marked as WIP

--- src/types/game_manager.ts
+++ src/types/game_manager.ts
@@ -1,43 +1,45 @@
-import { Block } from "./blockchain/block";
-import { Blockchain } from "./blockchain/blockchain";
-import { Transaction } from "./blockchain/transaction";
-import { BlockFactory } from "./block_factory";
-import { Miner } from "./miner/miner";
+export const wip = 1
 
-export class GameManager {
-    private blockchain: Blockchain
-    private miner: Miner
-    private neighbours: Set<string>
+// import { Block } from "./blockchain/block";
+// import { Blockchain } from "./blockchain/blockchain";
+// import { Transaction } from "./blockchain/transaction";
+// import { BlockFactory } from "./block_factory";
+// import { Miner } from "./miner/miner";
 
-    private focusedTransaction: Transaction
-    private blockFactory: BlockFactory
+// export class GameManager {
+//     private blockchain: Blockchain
+//     private miner: Miner
+//     private neighbours: Set<string>
 
-    constructor() {
+//     private focusedTransaction: Transaction
+//     private blockFactory: BlockFactory
 
-    }
+//     constructor() {
 
-    addTransactionToMempool = (transaction: Transaction) => {
-        this.miner.getMempool().put(transaction)
-    }
-    removeTransactionFromMempool = (hashString: string) => {
-        this.miner.getMempool().removeTransactionByHashString(hashString)
-    }
-    focusTransactionFromMempool = (hashString: string) => {
-        const removed = this.miner.getMempool().removeTransactionByHashString(hashString)
-        if (!removed) {
-            return
-        }
-        this.focusedTransaction = removed
-    }
+//     }
 
-    addNeighbour = (neighbourName: string) => {
-        this.neighbours.add(neighbourName)
-    }
-    removeNeighbour = (neighbourName: string) => {
-        this.neighbours.delete(neighbourName)
-    }
+//     addTransactionToMempool = (transaction: Transaction) => {
+//         this.miner.getMempool().put(transaction)
+//     }
+//     removeTransactionFromMempool = (hashString: string) => {
+//         this.miner.getMempool().removeTransactionByHashString(hashString)
+//     }
+//     focusTransactionFromMempool = (hashString: string) => {
+//         const removed = this.miner.getMempool().removeTransactionByHashString(hashString)
+//         if (!removed) {
+//             return
+//         }
+//         this.focusedTransaction = removed
+//     }
 
-    broadcastBlocks = (block: Block) => {
+//     addNeighbour = (neighbourName: string) => {
+//         this.neighbours.add(neighbourName)
+//     }
+//     removeNeighbour = (neighbourName: string) => {
+//         this.neighbours.delete(neighbourName)
+//     }
+
+//     broadcastBlocks = (block: Block) => {
         
-    }
-}
+//     }
+// }
