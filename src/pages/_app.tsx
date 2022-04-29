cd114f2054d4046e82eca517bbc5540a5e2a7c0d
Yushi Sato <yushi.kinger@gmail.com>
2022-04-30 01:47:59 +0900

    adding context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { BlockchainValidator } from '../consensus/blockchain_validator'
+import { TransactionVerifier } from '../consensus/transaction_verifiier'
+import { ConsensusEngine } from '../consensus/consensus_engine'
+import React from 'react'
+
+const verifier = new TransactionVerifier()
+const consensus = new ConsensusEngine()
+const defaultValidator = new BlockchainValidator(verifier, consensus)
+
+const BlockchainContext = React.createContext<BlockchainValidator>(defaultValidator)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BlockchainContext.Provider value={defaultValidator}>
+      <Component {...pageProps} />
+    </BlockchainContext.Provider>
+  )
 }
 
 export default MyApp
