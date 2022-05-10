23cba62cddf73d573d070225c1102144307af9a0
Yushi Sato <yushi.kinger@gmail.com>
2022-05-11 00:32:13 +0900

    add block api

---
+++ src/connection/block_api.ts
@@ -0,0 +1,53 @@
+import { BlockchainValidator } from "../consensus/blockchain_validator"
+import { ConsensusEngine } from "../consensus/consensus_engine"
+import { TransactionVerifier } from "../consensus/transaction_verifiier"
+import { Block } from "../types/blockchain/block"
+import { Blockchain } from "../types/blockchain/blockchain"
+
+
+export interface BlockApi {
+    tryAppendBlock(block: Block): Promise<boolean> 
+    getLatestHeight(): Promise<number>
+    getLatestBlock(): Promise<Block|undefined>
+    getBlockByHash(hashString: string): Promise<Block|undefined>
+}
+
+export class DefaultBlockApi implements BlockApi {
+    private validator: BlockchainValidator
+    private blockchain: Blockchain
+    private blocks: Map<string, Block>
+
+    constructor() {
+        const verifier = new TransactionVerifier()
+        const engine = new ConsensusEngine()
+        this.validator = new BlockchainValidator(verifier, engine)
+        this.blockchain = new Blockchain([])
+        this.blocks = new Map<string, Block>()
+    }
+
+    tryAppendBlock = async (block: Block): Promise<boolean> => {
+        const error = this.validator.tryAppendBlock(this.blockchain, block)
+        if(!error) {
+            return true    
+        }
+        this.blocks.set(block.hashString(), block) 
+        console.log('malformed block ', error.height, 'received:', error.message)
+        return false
+    }
+
+    getLatestHeight = async (): Promise<number> => {
+        return this.blockchain.blocks.length
+    }
+
+    getLatestBlock = async (): Promise<Block|undefined> => {
+        if (this.blockchain.blocks.length > 0) {
+            return this.blockchain.blocks[this.blockchain.blocks.length - 1]
+        }
+        return undefined
+    }
+
+    getBlockByHash = async (hashString: string): Promise<Block|undefined> => {
+        return this.blocks.get(hashString)
+    }
+}
+
--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,17 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { IncomingMessage, ServerResponse } from 'http'
 import { Server } from 'socket.io'
-import { BlockchainValidator } from '../../consensus/blockchain_validator'
-import { ConsensusEngine } from '../../consensus/consensus_engine'
-import { TransactionVerifier } from '../../consensus/transaction_verifiier'
+import { DefaultBlockApi } from '../../connection/block_api'
 import { Block } from '../../types/blockchain/block'
-import { Blockchain } from '../../types/blockchain/blockchain'
 
-const verifier = new TransactionVerifier()
-const engine = new ConsensusEngine()
-const validator = new BlockchainValidator(verifier, engine)
-
-const blockchain = new Blockchain([])
+const api = new DefaultBlockApi()
 
 const SocketHandler = (req: IncomingMessage, res: any) => {
     if (res.socket.server.io) {
@@ -26,10 +19,11 @@ const SocketHandler = (req: IncomingMessage, res: any) => {
                 socket.broadcast.emit('new-transaction', msg)
             })
 
-            socket.on('propagate', msg => {
+            socket.on('propagate', async msg => {
                 const block = Block.decode(Buffer.from(msg, 'hex'))
                 console.log(`block[${block.getHeight().toString()}]`, msg)
-                if(!validate(block)){
+                const ok = await api.tryAppendBlock(block)
+                if(!ok) {
                     return
                 }
                 socket.broadcast.emit('new-block', msg)
@@ -37,15 +31,6 @@ const SocketHandler = (req: IncomingMessage, res: any) => {
         })
     }
     res.end()
-}
-
-const validate = (block: Block): boolean => {
-    const error = validator.tryAppendBlock(blockchain, block)
-    if(!error) {
-        return true    
-    }
-    console.log('malformed block ', error.height, 'received:', error.message)
-    return false
 }
 
 export default SocketHandler
