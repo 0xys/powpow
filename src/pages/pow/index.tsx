b6371d58e2cf31648a47f72db146e408d391a5f8
Yushi Sato <yushi.kinger@gmail.com>
2022-06-10 02:01:54 +0900

    block header view that is editable

--- src/pages/pow/index.tsx
+++ src/pages/pow/index.tsx
@@ -1,10 +1,12 @@
-import { Box, VStack, HStack, Input, Button, Divider, Heading, ButtonGroup, IconButton, Textarea, InputGroup, InputLeftAddon, useToast, Alert, AlertIcon, AlertTitle, SimpleGrid, Center } from '@chakra-ui/react'
+import { Box, VStack, HStack, Input, Text, Button, Divider, Heading, ButtonGroup, IconButton, Textarea, InputGroup, InputLeftAddon, useToast, Alert, AlertIcon, AlertTitle, SimpleGrid, Center } from '@chakra-ui/react'
 import { AddIcon, CopyIcon } from '@chakra-ui/icons'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import crypto from 'crypto'
 import { createHash } from 'crypto';
 import { hexFont } from '../components/hex/hexOneline';
 import { HexOnelineView } from '../components/hex/hexOnelineView';
+import { BlockHeader } from '../../types/blockchain/blockHeader';
+import React from 'react';
 
 const Octet = (prop: {
   index: number,
@@ -23,20 +25,29 @@ const Octet = (prop: {
     </Center>)
 }
 
+const defaultBlockHeader = new BlockHeader(BigInt(1), BigInt(0), crypto.randomBytes(32), crypto.randomBytes(32), BigInt(0))
+
 export default function Pow() {
-  const [bytes, setBytes] = useState<Uint8Array>(Uint8Array.from(Buffer.allocUnsafe(80).fill(0)))
   const [selectionArray, setSelectionArray] = useState<boolean[]>()
   const [label, setLabel] = useState('')
   const [selectedBytes, setSelectedBytes] = useState<Uint8Array>()
+  const [blockHeader, setBlockHeader] = useState<BlockHeader>()
+
   const toast = useToast()
 
   useEffect(() => {
-    setBytes(Uint8Array.from(crypto.randomBytes(80)))
+    setBlockHeader(defaultBlockHeader)
   }, [])
 
-  const hexArray = useMemo(() => {
-    const buf = Buffer.from(bytes)
-    return buf.toJSON()
+  const [bytes, hexArray] = useMemo(() => {
+    let bytes: Buffer
+    if (!blockHeader) {
+      bytes = Buffer.alloc(80).fill(0)
+    }else{
+      bytes = blockHeader.encode()
+    }
+
+    const hexArray = bytes.toJSON()
       .data
       .map(v => {
         if (v < 16) {
@@ -45,7 +56,8 @@ export default function Pow() {
           return v.toString(16)
         }
       })
-  }, [bytes])
+    return [bytes, hexArray]
+  }, [blockHeader])
 
   const copy = (text: string) => {
     navigator.clipboard.writeText(text)
@@ -71,22 +83,22 @@ export default function Pow() {
       setSelectedBytes(bytes?.slice(0, 4) ?? Uint8Array.from([]))
     } else if (index < 8) {
       const array = Array.apply(null, Array(80)).map((v,i) => i >= 4 && i < 8)
-      setLabel('block height')
+      setLabel('height')
       setSelectionArray(array)
       setSelectedBytes(bytes?.slice(4, 8) ?? Uint8Array.from([]))
     } else if (index < 40) {
       const array = Array.apply(null, Array(80)).map((v,i) => i >= 8 && i < 40)
-      setLabel('previous block hash')
+      setLabel('prevBlockHash')
       setSelectionArray(array)
       setSelectedBytes(bytes?.slice(8, 40) ?? Uint8Array.from([]))
     } else if (index < 44) {
       const array = Array.apply(null, Array(80)).map((v,i) => i >= 40 && i < 44)
-      setLabel('difficulty target')
+      setLabel('diffTarget')
       setSelectionArray(array)
       setSelectedBytes(bytes?.slice(40, 44) ?? Uint8Array.from([]))
     } else if (index < 76) {
       const array = Array.apply(null, Array(80)).map((v,i) => i >= 44 && i < 76)
-      setLabel('merkle root')
+      setLabel('merkleRoot')
       setSelectionArray(array)
       setSelectedBytes(bytes?.slice(44, 76) ?? Uint8Array.from([]))
     } else {
@@ -100,16 +112,138 @@ export default function Pow() {
   const onLeave = () => {
     const array = Array.apply(null, Array(80)).map((v,i) => false)
     setSelectionArray(array)
+    setLabel('')
   }
+
+  const setVersion = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setVersion(b)
+    setBlockHeader({...blockHeader})
+  }
+  const setHeight = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setHeight(b)
+    setBlockHeader({...blockHeader})
+  }
+  const setPrevBlockHash = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setPrevBlockHash(b)
+    setBlockHeader({...blockHeader})
+  }
+  const setDifficultyTarget = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setDifficultyTarget(b)
+    setBlockHeader({...blockHeader})
+  }
+  const setMerkleRoot = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setMerkleRoot(b)
+    setBlockHeader({...blockHeader})
+  }
+  const setNonce = (b: Buffer) => {
+    if (!blockHeader) {
+      return
+    }
+    blockHeader.setNonce(b)
+    setBlockHeader({...blockHeader})
+  }
+
+  const hash = useMemo(() => {
+    return blockHeader?.hash()
+  }, [blockHeader])
 
   return (
     <VStack>
+      <HexOnelineEdit title='Version' hex={blockHeader?.getVersionBuffer()} byteLength={4} hexLength={68} setValue={setVersion} focused={label=='version'}/>
+      <HexOnelineEdit title='Height' hex={blockHeader?.getHeightBuffer()} byteLength={4} hexLength={68} setValue={setHeight} focused={label=='height'}/>
+      <HexOnelineEdit title='Prev Block Hash' hex={blockHeader?.getPrevBlockHash()} byteLength={32} hexLength={68} setValue={setPrevBlockHash} focused={label=='prevBlockHash'}/>
+      <HexOnelineEdit title='Difficulty' hex={blockHeader?.getDifficultyTargetBuffer()} byteLength={4} hexLength={68} setValue={setDifficultyTarget} focused={label=='diffTarget'}/>
+      <HexOnelineEdit title='Merkle Root' hex={blockHeader?.getMerkleRoot()} byteLength={32} hexLength={68} setValue={setMerkleRoot} focused={label=='merkleRoot'}/>
+      <HexOnelineEdit title='Nonce' hex={blockHeader?.getNonceBuffer()} byteLength={4} hexLength={68} setValue={setNonce} focused={label=='nonce'}/>
       <SimpleGrid columns={16} onMouseLeave={e => onLeave()}>
         {hexArray.map((v, i) => <Octet index={i} hex={v} selected={!!selectionArray? selectionArray[i]: false} onHover={onHover} key={i}/>)}
       </SimpleGrid>
       <Center>
-        <HexOnelineView title={label} hex={selectedBytes} size={68} copy={copy} titleLength={20}/>
+        <HexOnelineView title={'Block Hash'} hex={hash} size={68} copy={copy} titleLength={20}/>
       </Center>
     </VStack>
   )
 }
+
+
+const HexOnelineEdit = React.memo((prop: {
+  title: string,
+  hex?: Uint8Array,
+  byteLength: number,
+  hexLength?: number,
+  titleLength?: number,
+  focused: boolean,
+  setValue: (buf: Buffer) => void,
+}) => {
+  const { title, hex, hexLength, titleLength } = prop
+
+  const [err, setErr] = useState('')
+
+  const hexString = useMemo(() => {
+      return Buffer.from(hex ?? []).toString('hex')
+  }, [prop.hex])
+
+  const regex = useMemo(() => {
+    const regex = new RegExp(`^(0x)?([0-9A-Fa-f][0-9A-Fa-f]){${prop.byteLength}}$`)
+    return regex
+  }, [prop.byteLength])
+
+  const onValueChange = (e: any) => {
+    const value: string = e.target.value
+    if (regex.test(value)) {
+      if(value.startsWith('0x')) {
+        prop.setValue(Buffer.from(value.substring(2), 'hex'))
+      }else{
+        prop.setValue(Buffer.from(value, 'hex'))
+      }
+      setErr('')
+    }else{
+      if(value.startsWith('0x')) {
+        if(value.length == 2 + 2 * prop.byteLength) {
+          setErr('not hex')
+        }else{
+          setErr('wrong length')
+        }
+      }else{
+        if(value.length == 2 * prop.byteLength) {
+          setErr('not hex')
+        }else{
+          setErr('wrong length')
+        }
+      }
+      prop.setValue(Buffer.alloc(prop.byteLength).fill(0))
+    }
+  }
+
+  return (
+    <HStack>
+      <InputGroup size='sm' variant='outline'>
+        <InputLeftAddon width={`${titleLength ?? 20}ch`} children={title} fontWeight={prop.focused?'extrabold':'normal'} />
+        <Input
+          width={`${hexLength ?? hexString.length}ch`}
+          fontFamily={hexFont}
+          variant='filled'
+          defaultValue={hexString}
+          onChange={(e) => onValueChange(e)}
+          isInvalid={!!err}
+          errorBorderColor='crimson'/>
+      </InputGroup>
+      <Text fontSize='sm' color='tomato' display={err?'block':'none'}>{err}</Text>
+    </HStack>
+  )
+})
---
+++ src/types/blockchain/blockHeader.ts
@@ -0,0 +1,133 @@
+import { toBigIntBE, toBufferBE } from "bigint-buffer"
+import { createHash } from 'crypto';
+
+export class BlockHeader {
+    difficultyTarget: bigint
+    version: bigint
+    height: bigint
+    prevBlockHash: Buffer
+    merkleRoot: Buffer
+    nonce: bigint
+    constructor(
+        version: bigint,
+        height: bigint,
+        prevBlockHash: Buffer,
+        merkleRoot: Buffer,
+        nonce: bigint,
+        difficultyTarget?: bigint)
+    {
+        this.version = version
+        this.height = height
+        this.prevBlockHash = prevBlockHash
+        this.merkleRoot = merkleRoot
+        this.nonce = nonce
+
+        if (difficultyTarget) {
+            this.difficultyTarget = difficultyTarget
+        }else{
+            //  initial difficulty
+            this.difficultyTarget = toBigIntBE(Buffer.from([0xee, 0x00, 0x00, 0x00]))
+        }
+    }
+
+    // 4 byte
+    getVersion = (): bigint => {
+        return this.version
+    }
+    getVersionBuffer = (): Buffer => {
+        return toBufferBE(this.getVersion(), 4)
+    }
+    setVersion = (version: Buffer) => {
+        this.version = toBigIntBE(version)
+    }
+
+    // 4 byte
+    getHeight = (): bigint => {
+        return this.height
+    }
+    getHeightBuffer = (): Buffer => {
+        return toBufferBE(this.getHeight(), 4)
+    }
+    setHeight = (height: Buffer) => {
+        this.height = toBigIntBE(height)
+    }
+
+    // 32 byte
+    getPrevBlockHash = (): Buffer => {
+        return this.prevBlockHash
+    }
+    getPrevBlockHashString = (): string => {
+        return this.getPrevBlockHash().toString('hex')
+    }
+    setPrevBlockHash = (prevHash: Buffer) => {
+        this.prevBlockHash = prevHash
+    }
+
+    //  4 byte
+    getDifficultyTarget = (): bigint => {
+        return this.difficultyTarget
+    }
+    getDifficultyTargetBuffer = (): Buffer => {
+        return toBufferBE(this.difficultyTarget, 4)
+    }
+    setDifficultyTarget = (difficultyTarget: Buffer) => {
+        this.difficultyTarget = toBigIntBE(difficultyTarget)
+    }
+
+    // 32 byte
+    getMerkleRoot = (): Buffer => {
+        return this.merkleRoot
+    }
+    setMerkleRoot = (merkleRoot: Buffer) => {
+        this.merkleRoot = merkleRoot
+    }
+
+    // 4 byte
+    getNonce = (): bigint => {
+        return this.nonce
+    }
+    getNonceBuffer = (): Buffer => {
+        return toBufferBE(this.getNonce(), 4)
+    }
+    setNonce = (nonce: Buffer) => {
+        this.nonce = toBigIntBE(nonce)
+    }
+
+    encode = (): Buffer => {
+        let bufs: Buffer[] = []
+
+        bufs.push(this.getVersionBuffer())
+        bufs.push(this.getHeightBuffer())
+        bufs.push(this.prevBlockHash)
+        bufs.push(this.getDifficultyTargetBuffer())
+        bufs.push(this.merkleRoot)
+        bufs.push(this.getNonceBuffer())
+
+        return Buffer.concat(bufs)
+    }
+    
+    hash = (): Buffer => {
+        const hashed = createHash('sha256').update(this.encode()).digest()
+        return hashed
+    }
+
+    static decode = (blob: Buffer): BlockHeader => {
+        const versionBuf = blob.slice(0, 4)
+        const version = toBigIntBE(versionBuf)
+
+        const heightBuf = blob.slice(4, 8)
+        const height = toBigIntBE(heightBuf)
+
+        const prevBlockHash = blob.slice(8, 40)
+
+        const difficultyTargetBuf = blob.slice(40, 44)
+        const difficultyTarget = toBigIntBE(difficultyTargetBuf)
+
+        const merkleRoot = blob.slice(44, 76)
+
+        const nonceBuf = blob.slice(76, 80)
+        const nonce = toBigIntBE(nonceBuf)
+
+        return new BlockHeader(version, height, prevBlockHash, merkleRoot, nonce, difficultyTarget)
+    }
+}
